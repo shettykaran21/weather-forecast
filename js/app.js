794182c54b48f38fce0a432be4102ac52c39dca1
Karan Shetty <shettykaran2110@gmail.com>
2020-08-12 21:24:57 +0530

    Change location

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
-const weather = new Weather('Los Angeles');
+const weather = new Weather('Pune');
 const ui = new UI();
 
 document.addEventListener('DOMContentLoaded', getWeather);
+
+document.querySelector('.search__form').addEventListener('submit', (e) => {
+  const city = document.querySelector('.search__input').value;
+
+  // Change city
+  weather.changeLocation(city);
+
+  // Display weather again
+  getWeather();
+
+  e.preventDefault();
+});
 
 function getWeather() {
   weather
